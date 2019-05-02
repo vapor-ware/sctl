240dea1eada53ae744af96f846fbe9d9106b6336
Charles Butler <chuck@vapor.io>
2019-05-01 19:08:01 -0500

    Init

---
+++ README.md
@@ -0,0 +1,45 @@
+# Scuttle
+
+<img src="sctl.svg" width="150" height="150" alt="sctl - pronounced scuttle" />
+
+
+## Usage
+
+Scuttle aims to help you keep from creating security breaches by keeping secrets
+in your SCM in plain text. If you operate on Google Cloud, you don't have a lot
+of options available to you out of the gate for managing secrets.
+
+Scuttle uses KMS keys, and IAM policy to enforce the level of trust you need
+at your trust boundaries. No plain text is stored in the repository, only cipher
+text that is decryptable w/ an IAM user that has the appropriate permissions.
+
+### Installation
+
+```
+TBD
+```
+
+### Manage Secrets
+
+```
+$ echo $SCTL_KEY
+projects/my-project/locations/us/keyRings/operations-keyring/cryptoKeys/operations
+$ sctl add foo bar
+$ sctl list
+FOO
+$ cat .scuttle.json
+[
+ {
+  "name": "FOO",
+  "cypher": "CiQArcZm2GES73oHpipKV3UHUyFOUkPvWADrV/H6IssOIfVuh9wSKwDujG3UyRBnTFqciamPsK0x8UIaq6kzsYlhPoA9YHCzh0pd3KOJFpkvQqI=",
+  "created": "2019-05-01T19:08:58.959335955-05:00"
+ }
+]
+```
+
+## Acknowledgements
+
+Several tools like this have come before; sctl offers a polite hat-tip to
+- [99designs/aws-vault](https://github.com/99designs/aws-vault)
+- [banksimple/knox](#)
+- [bitnami/sealed-secrets](https://github.com/bitnami/sealed-secrets)
---
+++ main.go
@@ -0,0 +1,231 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/urfave/cli"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"strings"
+	"time"
+
+	cloudkms "cloud.google.com/go/kms/apiv1"
+	b64 "encoding/base64"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
+	exec "os/exec"
+)
+
+// Serialized secret
+// { "Name": "DATADOG_API_KEY", "cypertext": "0xD34DB33F", "created": "2019-05-01 13:01:27.189242799 -0500 CDT m=+0.000075907"}
+type Secret struct {
+	Name       string    `json:"name"`
+	Cyphertext string    `json:"cypher"`
+	Created    time.Time `json:"created"`
+}
+
+func addSecret(to_add Secret) {
+	// Adds or Updates a secret
+	var secrets = readSecrets()
+	for index, element := range secrets {
+		if element.Name == to_add.Name {
+			log.Printf("Removing entry %s", element.Name)
+			secrets[index] = secrets[len(secrets)-1] // copy last element to index i
+			secrets[len(secrets)-1] = Secret{}       // erase last element (zero value)
+			secrets = secrets[:len(secrets)-1]       // truncate slice
+		}
+	}
+	secrets = append(secrets, to_add)
+	writeSecrets(secrets)
+}
+
+func rmSecret(secret_name string) {
+	// Remove a secret from the scuttle.json
+	var secrets = readSecrets()
+	for index, element := range secrets {
+		if element.Name == secret_name {
+			log.Printf("Removing entry %s", element.Name)
+			secrets[index] = secrets[len(secrets)-1] // copy last element to index i
+			secrets[len(secrets)-1] = Secret{}       // erase last element (zero value)
+			secrets = secrets[:len(secrets)-1]       // truncate slice
+		}
+	}
+	writeSecrets(secrets)
+}
+
+func readSecrets() []Secret {
+	file, err := ioutil.ReadFile(".scuttle.json")
+	if err != nil {
+		return []Secret{}
+	}
+
+	var data []Secret
+	err = json.Unmarshal([]byte(file), &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+func writeSecrets(data []Secret) {
+	jsonData, _ := json.MarshalIndent(&data, "", " ")
+	mode := int(0660)
+	err := ioutil.WriteFile(".scuttle.json", jsonData, os.FileMode(mode))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "sctl"
+	app.Usage = "Manage secrets encrypted by KMS"
+	app.Version = "0.1.0"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "Enable debug messages",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "add",
+			Usage: "add secret",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "key",
+					EnvVar: "SCTL_KEY",
+					Usage:  "GCloud KMS Key URI",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				secret_name := c.Args().First()
+				plaintext := []byte(c.Args()[1])
+				cypher, err := encryptSymmetric(c.String("key"), plaintext)
+				if err != nil {
+					log.Fatal(err)
+				}
+				encoded := b64.StdEncoding.EncodeToString(cypher)
+				to_add := Secret{
+					Name:       strings.ToUpper(secret_name),
+					Cyphertext: encoded,
+					Created:    time.Now(),
+				}
+				addSecret(to_add)
+				return nil
+			},
+		},
+		{
+			Name:  "rm",
+			Usage: "rm a secret",
+			Action: func(c *cli.Context) error {
+				secret_name := strings.ToUpper(c.Args().First())
+				rmSecret(secret_name)
+				return nil
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "list known secrets",
+			Action: func(c *cli.Context) error {
+				var secrets []Secret
+				secrets = readSecrets()
+				for _, secret := range secrets {
+					fmt.Println(secret.Name)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "run",
+			Usage: "run a command with secrets exported as env",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "key",
+					EnvVar: "SCTL_KEY",
+					Usage:  "GCloud KMS Key URI",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				var secrets []Secret
+				to_run := c.Args().First()
+
+				secrets = readSecrets()
+				cmd := exec.Command(to_run)
+				cmd.Env = os.Environ()
+				for _, secret := range secrets {
+					// uncan the base64
+					decoded, _ := b64.StdEncoding.DecodeString(secret.Cyphertext)
+					// Decrypt the raw encrypted secret w/ kms
+					cypher, _ := decryptSymmetric(c.String("key"), decoded)
+					// Format the decrypted data for ENV consumption
+					skrt := fmt.Sprintf("%s=%s", secret.Name, cypher)
+					// Append it to the command exec environment
+					cmd.Env = append(cmd.Env, skrt)
+				}
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				err := cmd.Run()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// encrypt will encrypt the input plaintext with the specified symmetric key
+// example keyName: "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID"
+func encryptSymmetric(keyName string, plaintext []byte) ([]byte, error) {
+	ctx := context.Background()
+	client, err := cloudkms.NewKeyManagementClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build the request.
+	req := &kmspb.EncryptRequest{
+		Name:      keyName,
+		Plaintext: plaintext,
+	}
+	// Call the API.
+	resp, err := client.Encrypt(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Ciphertext, nil
+}
+
+// decrypt will decrypt the input ciphertext bytes using the specified symmetric key
+// example keyName: "projects/PROJECT_ID/locations/global/keyRings/RING_ID/cryptoKeys/KEY_ID"
+func decryptSymmetric(keyName string, ciphertext []byte) ([]byte, error) {
+	ctx := context.Background()
+	client, err := cloudkms.NewKeyManagementClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build the request.
+	req := &kmspb.DecryptRequest{
+		Name:       keyName,
+		Ciphertext: ciphertext,
+	}
+	// Call the API.
+	resp, err := client.Decrypt(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Plaintext, nil
+}
