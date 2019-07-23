b538b6f293b9571f77a082c1a63745c4caf7e838
Charles Butler <chuck@vapor.io>
2019-07-23 09:03:35 -0500

    Build the output artifact in the k8s builder
    - This was an artifact of a docker based build step in the synse tooling
      that was non applicable. Scrubbing to run in the same ci agent that
      runs the previous steps.
    
    - Bumping the tag to 0.8.2 for CI purposes, despite having no changes to
      the application

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "sctl"
 	app.Usage = "Manage secrets encrypted by KMS"
-	app.Version = "0.8.1"
+	app.Version = "0.8.2"
 
 	app.Commands = []cli.Command{
 		{
