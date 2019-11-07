e416556c537a920baaadb98ffab5720a8f78260e
Erick Daniszewski <edaniszewski@gmail.com>
2019-11-07 11:26:51 -0500

    add check for latest version after command completes successfully (#44)

--- go.mod
+++ go.mod
@@ -4,6 +4,14 @@ go 1.13
 
 require (
 	cloud.google.com/go v0.46.3
+	github.com/fatih/color v1.7.0
+	github.com/google/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/hashicorp/go-version v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/tcnksm/go-latest v0.0.0-20170313132115-e3007ae9052e
 	github.com/urfave/cli v1.22.1
+	golang.org/x/net v0.0.0-20191105084925-a882066a44e0 // indirect
 	google.golang.org/genproto v0.0.0-20191002211648-c459b9ce5143
 )
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/fatih/color"
+	"github.com/tcnksm/go-latest"
 	"github.com/urfave/cli"
 	"github.com/vapor-ware/sctl/commands"
 )
@@ -13,6 +15,29 @@ func main() {
 	app.Name = "sctl"
 	app.Usage = "Manage secrets encrypted by KMS"
 	app.Version = "1.0.0"
+
+	// TODO (etd): This functionality could be moved to utils or elsewhere, but since the
+	//   current version is defined here, we need to be within the app scope to get to it.
+	//   This could be alleviated by encoding the version elsewhere or using build-time
+	//   args to pass in version information.
+	app.After = func(context *cli.Context) error {
+		tag := &latest.GithubTag{
+			Owner:      "vapor-ware",
+			Repository: "sctl",
+		}
+
+		res, err := latest.Check(tag, app.Version)
+		if err != nil {
+			// A failure to check should not result in a failure to use the tool. Just
+			// log the issue and continue on.
+			log.Printf("failed to check for latest version: %v", err)
+			return nil
+		}
+		if res.Outdated {
+			color.Yellow("\nA new version of sctl is available: current=%s, latest=%s", app.Version, res.Current)
+		}
+		return nil
+	}
 
 	app.Commands = commands.BuildContextualMenu()
 
