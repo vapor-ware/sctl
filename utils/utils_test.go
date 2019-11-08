8fac5fcb8e5f381810a534a433ed22c18b007fa0
Charles Butler <chuck@vapor.io>
2019-11-08 15:17:24 -0600

    Update utils test to exercise more of the AddSecret() codepath

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -30,13 +30,15 @@ func TestAddSecretHelper(t *testing.T) {
 	// Test New Secret in New Context
 	tempPath, tempPathErr := ioutil.TempDir("", "TestAddSecretHelper")
 	currPath, _ := os.Getwd()
-
+	t.Logf("Using temporary path %v", tempPath)
 	if tempPathErr != nil {
 		t.Skipf("Failed to create temporary path with error: %v", tempPathErr)
 	}
 	defer os.RemoveAll(tempPath)
 	os.Chdir(tempPath)
 
+	currWD, _ := os.Getwd()
+	t.Logf("In current path %v", currWD)
 	hush := Secret{
 		Name:       "TEST",
 		Cyphertext: "TESTCASEADDSECRET",
@@ -57,6 +59,14 @@ func TestAddSecretHelper(t *testing.T) {
 	if len(state) != 0 {
 		t.Errorf("Unexpected Slice Length, Wanted: 0, Got: %v", len(state))
 	}
+
+	// Now rotate Hush so we exercise the full codePath of AddSecret()
+	AddSecret(hush, "", true)
+
+	// now declare a key uri and try to change it
+	AddSecret(hush, "foo", true)
+
+	// TODO: Validate the KeyError Fatal messaging / codepath. testing log.Fatal is hard.
 
 	os.Chdir(currPath)
 }
